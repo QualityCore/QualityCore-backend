d99abedc28b7778d394f5b7f54560707e9d3e9ea
KIM-KWANHUN <rhksgns4@gmail.com>
2025-02-10 17:52:16 +0900

    백앤드 기본파일셋팅

--- src/main/java/com/org/qualitycore/config/QualityCoreApplication.java
+++ src/main/java/com/org/qualitycore/config/QualityCoreApplication.java
@@ -1,9 +1,13 @@
-package qualitycore;
+package com.org.qualitycore.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.org.qualitycore")
+@EnableJpaRepositories(basePackages = "com.org.qualitycore")
 public class QualityCoreApplication {
 
     public static void main(String[] args) {
