2e47b75b80a8ac8d81cdcafda66007548820727f
answjdgus72 <answjdgus72@gmail.com>
2025-02-10 14:53:26 +0900

    no message

---
+++ src/main/java/qualitycore/QualityCoreApplication.java
@@ -0,0 +1,13 @@
+package qualitycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QualityCoreApplication.class, args);
+    }
+
+}
