aea1e322d569229f85793e5e3ffea4da93d94c02
KIM-KWANHUN <rhksgns4@gmail.com>
2025-02-11 17:51:02 +0900

    <25.02.11> Swagger 처리

--- src/main/java/com/org/qualitycore/config/CorsConfig.java
+++ src/main/java/com/org/qualitycore/config/CorsConfig.java
@@ -1,16 +1,19 @@
 package com.org.qualitycore.config;
 
-import org.modelmapper
-
-        .ModelMapper;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan(basePackages = "com.org.qualitycore")
+@EnableWebMvc
 public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -25,6 +28,7 @@ public class CorsConfig {
         };
     }
 
+    // modelmapper 설정
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
@@ -35,4 +39,15 @@ public class CorsConfig {
 
         return modelMapper;
     }
+
+    //swagger 설정
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI().components(new Components()).info(swaggerInfo());
+    }
+    private Info swaggerInfo() {
+        return new Info().title("Ohgiraffers API").description("Swagger 연동 Test").version("1.0.0");
+    }
+
 }
