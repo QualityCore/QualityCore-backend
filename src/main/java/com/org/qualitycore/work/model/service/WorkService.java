e92c99d19ad82316d0844a9647a142bd32cb4aa7
KIM-KWANHUN <rhksgns4@gmail.com>
2025-02-11 11:57:02 +0900

    <25.02.11> 작업지시서 상세 조회

--- src/main/java/com/org/qualitycore/work/model/service/WorkService.java
+++ src/main/java/com/org/qualitycore/work/model/service/WorkService.java
@@ -17,6 +17,7 @@ public class WorkService {
     private final WorkRepository workRepository;
     private final ModelMapper modelMapper;
 
+    // 작업지시서 메인화면 전체조회
     public List<WorkDTO> findAllWorkOrders() {
 
         List<Work> work = workRepository.findAll();
@@ -24,4 +25,11 @@ public class WorkService {
         return work.stream().map(works -> modelMapper.map(works, WorkDTO.class)).collect(Collectors.toList());
     }
 
+
+    public WorkDTO findByWorkOrderCode(int workId) {
+
+        Work work = workRepository.findById(workId).orElseThrow(IllegalArgumentException::new);
+
+        return modelMapper.map(work, WorkDTO.class);
+    }
 }
