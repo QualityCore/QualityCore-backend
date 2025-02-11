4d533db4663a7e64be27de544f6f809847aec188
rlaskarb <rlaskarb20@gmail.com>
2025-02-11 11:30:14 +0900

    2/11 작업정보등록 등록기능 구현

--- src/main/java/com/org/qualitycore/masterdata/controller/MasterDataController.java
+++ src/main/java/com/org/qualitycore/masterdata/controller/MasterDataController.java
@@ -1,36 +1,52 @@
 package com.org.qualitycore.masterdata.controller;
 
+import com.org.qualitycore.masterdata.model.dto.WorkplaceDTO;
+import com.org.qualitycore.masterdata.model.entity.MasterDataMessage;
 import com.org.qualitycore.masterdata.model.entity.Workplace;
 import com.org.qualitycore.masterdata.model.service.MasterDataService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/masterdata")
 @RequiredArgsConstructor
 public class MasterDataController {
 
-    private  final MasterDataService masterDataService;
-
+    private final MasterDataService masterDataService;
+    private final ModelMapper modelMapper;
 
 
     // 작업장 전체 조회
     @GetMapping("/workplaces")
-    public List<Workplace> getAllWorkplaces(){
-        return masterDataService.getAllWorkplaces();
+    public List<WorkplaceDTO> getAllWorkplaces() {
+        List<Workplace> workplaces = masterDataService.getAllWorkplaces();
+        return workplaces.stream()
+                .map(workplace -> modelMapper.map(workplace, WorkplaceDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    // 작업장 등록
+    @PostMapping("/workplaces")
+    public ResponseEntity<MasterDataMessage> createWorkplace(@RequestBody WorkplaceDTO workplaceDTO) {
+        Workplace savedWorkplace = masterDataService.creactWorkplace(workplaceDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new MasterDataMessage(HttpStatus.CREATED.value(),
+                        "등록에 성공했어!! 축하해! ID :" + savedWorkplace.getWorkplaceId()));
     }
 
 
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
